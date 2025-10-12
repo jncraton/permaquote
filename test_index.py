d04d2ee0d9859e8ef9770597ce3f64bd13aec940
Jon Craton <jncraton@gmail.com>
2025-10-12 17:11:56 -0400

    Add test_index.py

---
+++ test_index.py
@@ -0,0 +1,11 @@
+import re
+from playwright.sync_api import Page, expect
+
+from pathlib import Path
+
+file_url = Path("index.html").resolve().as_uri()
+
+def test_quote(page: Page):
+    page.goto(f"{file_url}#https://example.com;Example;c1Qozy/KzsxLV0itSMwtyEkFAA==")
+
+    assert "A working example" in page.text_content("body")
