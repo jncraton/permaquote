a4989607f0ee8a74a6e4ee8ee52d677fd58b17e2
Jon Craton <jncraton@gmail.com>
2025-10-12 21:14:17 -0400

    Add date to test

--- test_index.py
+++ test_index.py
@@ -6,6 +6,6 @@ from pathlib import Path
 file_url = Path("index.html").resolve().as_uri()
 
 def test_quote(page: Page):
-    page.goto(f"{file_url}#https://example.com;Example;c1Qozy/KzsxLV0itSMwtyEkFAA==")
+    page.goto(f"{file_url}#https://example.com;Example;1970-01-01;c1Qozy/KzsxLV0itSMwtyEkFAA==")
 
     assert "A working example" in page.text_content("body")
