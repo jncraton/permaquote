b5eb3d3897d6d8a9b4f16218e4ba3d91cb094fb4
Jon Craton <jncraton@gmail.com>
2025-10-13 15:13:28 -0400

    Add shared test fixture

--- test_index.py
+++ test_index.py
@@ -1,11 +1,15 @@
 import re
 from playwright.sync_api import Page, expect
+import pytest
 
 from pathlib import Path
 
 file_url = Path("index.html").resolve().as_uri()
 
-def test_quote(page: Page):
+@pytest.fixture
+def example_page(page: Page):
     page.goto(f"{file_url}#https://example.com;Example;1970-01-01;c1Qozy/KzsxLV0itSMwtyEkFAA==")
+    return page
 
-    assert "A working example" in page.text_content("body")
+def test_quote(example_page):
+    assert "A working example" in example_page.text_content("body")
