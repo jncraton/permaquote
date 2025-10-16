9617af04d2cfdb8eff09861420d121ac5e394f59
Jon Craton <jncraton@gmail.com>
2025-10-16 18:13:53 -0400

    Test correct page title modification

--- test_index.py
+++ test_index.py
@@ -11,6 +11,11 @@ def example_page(page: Page):
     page.goto(f"{file_url}#https://example.com;The+Title;1970-01-01;c1Qozy/KzsxLV0itSMwtyEkFAA==")
     return page
 
+@pytest.fixture
+def blank_page(page: Page):
+    page.goto(f"{file_url}")
+    return page
+
 def test_page_title(example_page):
     expect(example_page).to_have_title("The Title")
 
@@ -28,3 +33,8 @@ def test_href(example_page):
 
 def test_quote(example_page):
     expect(example_page.get_by_text("A working example")).to_be_visible()
+
+def test_create_page_title(blank_page):
+    blank_page.get_by_label("Title").fill("The Title")
+
+    expect(blank_page).to_have_title("The Title")
