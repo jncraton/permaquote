6211dd91f9e1365ef079e44922feebb8e6fb21e0
Jon Craton <jncraton@gmail.com>
2025-10-17 18:44:40 -0400

    Load book metadata

--- main.js
+++ main.js
@@ -73,6 +73,40 @@ class URIAnchor extends HTMLElement {
 
 customElements.define('uri-anchor', URIAnchor)
 
+class URIInfo extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
+
+  connectedCallback() {
+    this.attributeChangedCallback()
+  }
+
+  async attributeChangedCallback(name, oldValue, newValue) {
+    if (this.attributes.uri?.value.startsWith('urn:isbn:')) {
+      const isbn = this.attributes.uri?.value.slice(9)
+      const p = document.createElement('p')
+      p.textContent = `Loading metadata...`
+      this.shadowRoot.replaceChildren(p)
+      try {
+        const res = await fetch(`https://openlibrary.org/search.json?q=isbn:${isbn}`)
+        const result = await res.json()
+
+        const title = result.docs[0].title
+        const author = result.docs[0].author_name[0]
+        const year = result.docs[0].first_publish_year
+
+        p.textContent = ` ${year} ${author}`
+      } catch {
+        p.textContent = `Unable to load metadata`
+      }
+    }
+  }
+}
+
+customElements.define('uri-info', URIInfo)
+
 async function encode(text, algo) {
   let stream = new Blob([text]).stream()
 
@@ -162,7 +196,7 @@ async function render(hash) {
   template.querySelector('time').textContent = template.querySelector('time').datetime
 
   let text_fragment = encodeURIComponent(text.slice(0, 50).replace(/\W*\w+$/, ''))
-  template.querySelectorAll('uri-anchor,uri-thumb').forEach(el => {
+  template.querySelectorAll('uri-anchor,uri-thumb,uri-info').forEach(el => {
     el.setAttribute('uri', src_url)
   })
 
