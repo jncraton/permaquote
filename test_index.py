c446caca7b601cbb3cd10d5678e9840ad5a35542
Jon Craton <jncraton@gmail.com>
2025-10-17 13:11:59 -0400

    Simplify variable names

--- test_index.py
+++ test_index.py
@@ -7,34 +7,34 @@ from pathlib import Path
 file_url = Path("index.html").resolve().as_uri()
 
 @pytest.fixture
-def example_page(page: Page):
+def web(page: Page):
     page.goto(f"{file_url}#https://example.com;The+Title;1970-01-01;c1Qozy/KzsxLV0itSMwtyEkFAA==")
     return page
 
 @pytest.fixture
-def blank_page(page: Page):
+def blank(page: Page):
     page.goto(f"{file_url}")
     return page
 
-def test_page_title(example_page):
-    expect(example_page).to_have_title("The Title")
+def test_page_title(web):
+    expect(web).to_have_title("The Title")
 
-def test_title(example_page):
-    expect(example_page.get_by_text("The Title")).to_be_visible()
+def test_title(web):
+    expect(web.get_by_text("The Title")).to_be_visible()
 
-def test_date(example_page):
-    expect(example_page.get_by_text("1970-01-01")).to_be_visible()
+def test_date(web):
+    expect(web.get_by_text("1970-01-01")).to_be_visible()
 
-def test_link(example_page):
-    expect(example_page.get_by_text("example.com").first).to_be_visible()
+def test_link(web):
+    expect(web.get_by_text("example.com").first).to_be_visible()
 
-def test_href(example_page):
-    expect(example_page.get_by_text("example.com").first).to_have_attribute("href", re.compile("https://example.com.*"))
+def test_href(web):
+    expect(web.get_by_text("example.com").first).to_have_attribute("href", re.compile("https://example.com.*"))
 
-def test_quote(example_page):
-    expect(example_page.get_by_text("A working example")).to_be_visible()
+def test_quote(web):
+    expect(web.get_by_text("A working example")).to_be_visible()
 
-def test_create_page_title(blank_page):
-    blank_page.get_by_label("Title").fill("The Title")
+def test_create_page_title(blank):
+    blank.get_by_label("Title").fill("The Title")
 
-    expect(blank_page).to_have_title("The Title")
+    expect(blank).to_have_title("The Title")
