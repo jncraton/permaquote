8d8a662f35c1a594123b37c4126466c68e819a1c
Jon Craton <jncraton@gmail.com>
2026-07-26 06:38:00 -0400

    Inline all js

--- pack.py
+++ pack.py
@@ -2,7 +2,7 @@ import re
 
 template = open('index.template.html').read()
 style = open('style.css').read()
-script = open('main.js').read()
+script = open('urinfo.js').read() + open('camel.js').read() + open('main.js').read()
 
 template = template.replace('<link rel="stylesheet" href="style.css" type="text/css" />', f"<style>{style}</style>")
 template = template.replace('<script src="main.js"></script>', f"<script>{script}</script>")
