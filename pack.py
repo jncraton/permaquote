a6e0e53a3f550a8f65fcb6e0ab9e9869efb59c06
Jon Craton <jncraton@gmail.com>
2026-07-28 15:47:08 -0400

    Allow modules

--- pack.py
+++ pack.py
@@ -6,7 +6,7 @@ template = template.replace('<link rel="stylesheet" href="style.css" type="text/
 for jsfile in ['urinfo.js', 'camel.js', 'main.js']:
     script = open(jsfile).read()
 
-    template = template.replace(f'<script src="{jsfile}"></script>', f"<script>{script}</script>")
+    template = template.replace(f' src="{jsfile}"></script>', f">{script}</script>")
 
 with open('index.html', 'w') as f:
     f.write(template)
